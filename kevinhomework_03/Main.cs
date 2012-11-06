db2ac3c6effaee92af1be3b5b83fa9f6b08e497f
hyungunoh <ohhyungun@gmail.com>
2012-11-06 21:22:23 +0900

    Robot Class 생성자 추가

--- kevinhomework_03/Main.cs
+++ kevinhomework_03/Main.cs
@@ -13,10 +13,8 @@ namespace kevinhomework_02
 			Random random = new Random();
 			
 			/// robot initializing here...
-			Robot robot1 = new Robot ();
-			robot1.hp = random.Next(8,11);
-			robot1.name = "Robot";
-			robot1.num = random.Next(1,11);
+			Robot robot1 = new Robot ("Robot");
+			
 			
 			RoChoice = random.Next(1,4);
 			
@@ -64,14 +62,31 @@ namespace kevinhomework_02
 public class Robot
 {
 	public int hp;
-	public string name;
-	public int num;
+	protected string _name;
+	protected int _num;
+	
+	public string name
+	{
+		get { return _name; }
+	}
+	
+	public int num
+	{
+		get { return _num; }
+	}
 	
 	public bool isDead
 	{
 		get { return hp <= 0; }
 	}
-
+	
+	public Robot(string newName)
+	{
+		Random r = new Random();
+		hp = r.Next(8,11);
+		_name = "Robot";
+		_num = r.Next(1,11);
+	}
 };
 
 
